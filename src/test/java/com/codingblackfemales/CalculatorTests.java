8e3820aa65c00927329172e723e737392bbc6367
Bariyat Eyiowuawi <taiwoeyiowuawi@gmail.com>
2023-10-12 21:48:29 +0100

    division tests added

--- src/test/java/com/codingblackfemales/CalculatorTests.java
+++ src/test/java/com/codingblackfemales/CalculatorTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName(value = "The Calculator should")
 public class CalculatorTests {
@@ -22,5 +23,38 @@ public class CalculatorTests {
         final Integer difference = Calculator.subtract(2, 4);
 
         assertEquals(-2, difference);
+    }
+
+    @Test
+    @DisplayName("divide two numbers")
+    public void testDivide() {
+        final Integer result = Calculator.divide(4, 2);
+
+        assertEquals(2, result);
+    }
+
+    @Test
+    @DisplayName("divide one negative number by  a positive number")
+    public void testDivideTwo() {
+        final Integer result = Calculator.divide(4, -2);
+
+        assertEquals(-2, result);
+    }
+
+    @Test
+    @DisplayName("divide two negative numbers")
+    public void testDivideThree() {
+        final Integer result = Calculator.divide(-4, -2);
+
+        assertEquals(2, result);
+    }
+
+    @Test
+    @DisplayName("divide by zero throws exception")
+    public void testDivideByZero() {
+        //final Integer product = Calculator.multiply(2, 4);
+
+        assertThrows(ArithmeticException.class,() -> {
+            Calculator.divide(4, 0);});
     }
 }
